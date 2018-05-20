c7538dca9f781fb87b10d0728e06246bcbd8ade1
thomaspihet1 <thomaspihet1@gmail.com>
2018-05-20 16:56:00 +0200

    test premier

--- application/views/v_acceuil.php
+++ application/views/v_acceuil.php
@@ -12,10 +12,7 @@
         <h1 style="text-align: center;"> GSB </h1>
     </head>
     <body>
-          <h3>Gestion des composants</h3>
-          <input id="btnListerComposant"  type="button" value="Liste des composants" onClick="location.href='../../index.php/Ctrl_Composant/afficherComposant'"/>
-          <input id="btnAjouterComposant"  type="button" value="Ajouter un composant" onclick="location.href='../../index.php/Ctrl_Composant/ajouterComposant'"/>
-          <input id="btnModifierComposant"  type="button" value="Modifier un composant" onclick="location.href='../../index.php/Ctrl_Composant/modifierComposant'"/>
+          
           <h3>Gestion des composants d'un médicament</h3>
           <input id="btnListeComposantDunMedoc"  type="button" value="Liste des composants d'un médicament" onclick="location.href='../../index.php/Ctrl_Composant/afficherMedicaments'"/>
           <input id="btnAjoutCompoMedoc"  type="button" value="Ajouter des composants à un médicament" onclick="location.href='../../index.php/Ctrl_Composant/ajouterComposantMeds'"/>
@@ -23,7 +20,9 @@
           <h3>Gestion des présentations</h3>
           <input id="btnAjoutPresDunMedoc"  type="button" value="Ajouter une présentation à un medicament" onclick="location.href='../../index.php/Ctrl_Composant/formulationMed'"/>
           <input id="btnAjoutPres"  type="button" value="Ajouter une présentation" onclick="location.href='../../index.php/Ctrl_Composant/ajouterPresentation'"/>
-          <h3>Statistiques</h3>
-          <input id="btnStats"  type="button" value="Voir les statistiques" onclick="location.href='../../index.php/Ctrl_Composant/statistique'"/>
+          <h3>Gestion des composants</h3>
+          <input id="btnListerComposant"  type="button" value="Liste des composants" onClick="location.href='../../index.php/Ctrl_Composant/afficherComposant'"/>
+          <input id="btnAjouterComposant"  type="button" value="Ajouter un composant" onclick="location.href='../../index.php/Ctrl_Composant/ajouterComposant'"/>
+          <input id="btnModifierComposant"  type="button" value="Modifier un composant" onclick="location.href='../../index.php/Ctrl_Composant/modifierComposant'"/>
     </body>
 </html>
